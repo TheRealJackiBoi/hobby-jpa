28394a275fdd083b724a8ebd81e979cd4f20c539
artiecodes <bjarke.dehlie@gmail.com>
2023-09-05 10:56:22 +0200

    Added Singleton EMF, Test&Code for US-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,3 @@
-import dao.EMF;
-import model.Address;
-import model.City;
-import model.Phonenumber;
-import model.Users;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
